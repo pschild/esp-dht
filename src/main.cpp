ef4a1dcbc44519682c3efa0be0db8184148fe8a2
pschild <philippe.schild@googlemail.com>
2021-05-17 14:19:11 +0200

    read and publish DHT values and voltage

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <OTAUpdateHandler.h>
 #include <ESP8266HTTPClient.h>
 #include <dht.h>
+#include <ArduinoJson.h>
 
 #ifndef WIFI_SSID
   #error "Missing WIFI_SSID"
@@ -20,6 +21,12 @@
 #endif
 
 const String CHIP_ID = String("ESP_") + String(ESP.getChipId());
+
+const String dhtChannel = String("devices/") + CHIP_ID + String("/dht");
+const String tempChannel = String("devices/") + CHIP_ID + String("/temp");
+const String humChannel = String("devices/") + CHIP_ID + String("/hum");
+const String voltageChannel = String("devices/") + CHIP_ID + String("/voltage");
+const String debugChannel = String("devices/") + CHIP_ID + String("/debug");
 
 // void ping();
 void publishDhtValues();
@@ -36,10 +43,14 @@ MqttHandler mqttHandler("192.168.178.28", CHIP_ID);
 OTAUpdateHandler updateHandler("192.168.178.28:9042", VERSION);
 
 // Ticker pingTimer(ping, 60 * 1000);
-Ticker deepSleepTimer(startDeepSleep, 10 * 1000); // 10s
+Ticker publishDhtTimer(publishDhtValues, 1000, 5); // every second, 5 times max
+Ticker deepSleepTimer(startDeepSleep, 15 * 1000); // start deepsleep 15s after boot
 
 dht DHT;
 #define DHT22_PIN 4 // ~D2 on NodeMCU / GPIO4 on ESP12F
+boolean dhtPublished = false;
+
+ADC_MODE(ADC_VCC); // allows you to monitor the internal VCC level
 
 void setup() {
   Serial.begin(9600);
@@ -62,6 +73,7 @@ void loop() {
   mqttHandler.loop();
   updateHandler.loop();
   // pingTimer.update();
+  publishDhtTimer.update();
   deepSleepTimer.update();
 }
 
@@ -78,18 +90,50 @@ void ledTurnOff() {
   mqttHandler.publish(channel.c_str(), VERSION);
 }*/
 
+void publishVoltageLevel() {
+  float volts = ESP.getVcc();
+  mqttHandler.publish(voltageChannel.c_str(), String(volts / 1024.00f).c_str());
+  mqttHandler.publish(debugChannel.c_str(), String(volts).c_str());
+}
+
+String buildJsonDoc(float temperature, float humidity) {
+  StaticJsonDocument<64> jsonDoc;
+  jsonDoc["temperature"] = temperature;
+  jsonDoc["humidity"] = humidity;
+  String output;
+  serializeJson(jsonDoc, output);
+  return output;
+}
+
 void publishDhtValues() {
-  DHT.read22(DHT22_PIN);
+  if (dhtPublished == true) {
+    // values were already published
+    return;
+  }
 
-  const String tempChannel = String("devices/") + CHIP_ID + String("/temp");
-  mqttHandler.publish(tempChannel.c_str(), String(DHT.temperature).c_str());
-
-  const String humChannel = String("devices/") + CHIP_ID + String("/hum");
-  mqttHandler.publish(humChannel.c_str(), String(DHT.humidity).c_str());
+  int chk = DHT.read22(DHT22_PIN);
+  switch (chk) {
+    case DHTLIB_OK:
+      mqttHandler.publish(debugChannel.c_str(), "DHTLIB_OK");
+      mqttHandler.publish(tempChannel.c_str(), String(DHT.temperature).c_str());
+      mqttHandler.publish(humChannel.c_str(), String(DHT.humidity).c_str());
+      mqttHandler.publish(dhtChannel.c_str(), buildJsonDoc(DHT.temperature, DHT.humidity).c_str());
+      dhtPublished = true;
+      break;
+    case DHTLIB_ERROR_CHECKSUM:
+      mqttHandler.publish(debugChannel.c_str(), "DHTLIB_ERROR_CHECKSUM");
+      break;
+    case DHTLIB_ERROR_TIMEOUT:
+      mqttHandler.publish(debugChannel.c_str(), "DHTLIB_ERROR_TIMEOUT");
+      break;
+    default:
+      mqttHandler.publish(debugChannel.c_str(), "UNKNOWN ERROR");
+      break;
+  }
 }
 
 void startDeepSleep() {
-  ESP.deepSleep(5 * 60e6); // 5min
+  ESP.deepSleep(15 * 60e6); // sleep for 15min
   delay(100);
 }
 
@@ -109,7 +153,8 @@ void onMqttConnected() {
   mqttHandler.subscribe("foo/+/baz");
   mqttHandler.subscribe("otaUpdate/all");
 
-  publishDhtValues();
+  publishVoltageLevel();
+  publishDhtTimer.start();
   deepSleepTimer.start(); // start deep sleep timer to allow enough time for the dht values to be sent
 }
 
